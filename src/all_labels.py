5b52ba10233bbe59d94b1a875f8d1e5b525e9506
qiuqiu035 <169140248+qiuqiu035@users.noreply.github.com>
2025-09-17 16:04:39 +0200

    Remove unnecessary comments and clean up code

--- src/all_labels.py
+++ src/all_labels.py
@@ -1,7 +1,5 @@
 import os
 
-# 设置主数据路径
-root_dir = r"C:\Users\vghy0\OneDrive\Desktop\final\vision\FOOD_HEALTH_ANALYSIS\data\UECFOOD256"
 output_file = os.path.join(root_dir, "all_labels.txt")
 
 with open(output_file, "w") as out_f:
@@ -18,10 +16,9 @@ with open(output_file, "w") as out_f:
                 if len(parts) != 5:
                     continue
                 if parts[0] == "img" or parts[1] == "x1":
-                    continue  # ✅ 跳过表头或伪装行
+                    continue 
                 img_name, x1, y1, x2, y2 = parts
                 img_id = os.path.splitext(img_name)[0]
                 out_f.write(f"{img_id} {class_folder - 1} {x1} {y1} {x2} {y2}\n")
 
-print("✅ all_labels.txt 已重新生成并清洗")
 
